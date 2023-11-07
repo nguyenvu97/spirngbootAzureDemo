44a4a9462eb1603f7e98bd3b60c49787de060812
nguyenvu97 <137592517+nguyenvu97@users.noreply.github.com>
2023-11-07 14:43:08 +0700

    first commit

---
+++ src/test/java/com/example/aruretestgithub/ArureTestGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aruretestgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArureTestGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
